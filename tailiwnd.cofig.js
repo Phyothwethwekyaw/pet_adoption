c397225e2b5d31f1ee520bdedc38927df63e55c1
Phyothwethwekyaw <153508951+Phyothwethwekyaw@users.noreply.github.com>
2025-06-05 20:45:03 +0700

    Complete volunteer page

---
+++ tailiwnd.cofig.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export const themeConfig = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+};
