07ad9d7ffad2feb33f3d7751babd75c20087ec65
Phyothwethwekyaw <153508951+Phyothwethwekyaw@users.noreply.github.com>
2025-07-01 19:59:47 +0700

    modified

--- tailiwnd.cofig.js
+++ tailiwnd.cofig.js
@@ -1,6 +1,13 @@
 // tailwind.config.js
 export const themeConfig = {
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       fontFamily: {
         sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
